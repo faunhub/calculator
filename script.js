c27efa93892bb489d16bb26f2f1d14768faa1a7e
faunhub <76247482+faunhub@users.noreply.github.com>
2022-01-28 19:20:00 +0700

    fixed js

--- script.js
+++ script.js
@@ -14,10 +14,32 @@ function divide(a,b){
     return a/b;
 }
 
-// function operate(){
+function currentNumber(number){
+    if (display.textContent.length >= 14){
+        return;
+    }
+    display.textContent += number.textContent;
+    currentInt = display.textContent;
+    return display.textContent;
+}
 
-// }
+function operate(){
+    let a;
+    let b;
+    if (typeof(currentEquation[length-1]) == 'number'){
+        a = currentEquation[0];
+        b = currentEquation[length-1];
+        if (currentEquation[1] == 'add'){
+            currentEquation =[];
+            return add(a,b);
+        }
+    } else {
+        return;
+    }
+}
 
+//main
+const allOperation = document.querySelectorAll('.operation');
 const plus = document.querySelector('.add');
 const minus = document.querySelector('.subtract');
 const times = document.querySelector('.multiply');
@@ -25,13 +47,21 @@ const over = document.querySelector('.divide');
 const allNumber = document.querySelectorAll('.number');
 const display = document.querySelector('.display');
 const clear = document.querySelector('.clear');
-let currentTotal =0;
+let currentInt =0;
+let currentEquation = [];
+let result =0;
 
-allNumber.forEach(number => number.addEventListener('click',
-    () => {
-        if (display.textContent.length >= 14){
-            return;
-        }
-        display.textContent += number.textContent;
-    }));
+allNumber.forEach(number => number.addEventListener('click', () =>{
+    currentNumber(number);
+} ));
+allOperation.forEach(operation => operation.addEventListener('click',
+(operation) => {
+    // result = operate();
+    currentInt =0;
+    display.innerText ="";
+    currentEquation.push(operation.target.classList.item(1));
+    console.log(currentEquation);
+} 
+));
+
 clear.addEventListener('click', () => display.textContent ="");
